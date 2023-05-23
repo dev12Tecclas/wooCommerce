2ebbf4dd186a0c921c219404e5d506a3505a1759
dev12Tecclas <129604075+dev12Tecclas@users.noreply.github.com>
2023-05-23 13:37:12 -0500

    otro

--- extension.php
+++ extension.php
@@ -1,6 +1,11 @@
 <?php 
-
-$salida = shell_exec('node prueba.js')
+/**
+ * Plugin Name: Mi Extensión de WooCommerce
+ * Description: Una descripción de tu extensión.
+ * Version: 1.0.0
+ * Author: Tu Nombre
+ */
+$salida = shell_exec('node /prueba.js')
 echo $salida
 
 ?>
