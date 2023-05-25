16671418c48afb8e95bed0d9cf2d97620f03a507
dev12Tecclas <129604075+dev12Tecclas@users.noreply.github.com>
2023-05-25 11:21:44 -0500

    asd

--- prueba.js
+++ prueba.js
@@ -4,17 +4,18 @@ window.alert('prueba')
 document.addEventListener('DOMContentLoaded', function (){
     let body = document.body
     let newDiv = document.createElement('div')
-    newDiv.innerHTML = 'hola soy germna'
+
+    newDiv.innerHTML = './index.html'
 
     body.appendChild(newDiv)
 
 })
 
 
-urlArchivoHTML = '/index.html';
+// urlArchivoHTML = '/index.html';
 
-// Redireccionar al archivo HTML
-window.location.href = urlArchivoHTML;
+// // Redireccionar al archivo HTML
+// window.location.href = urlArchivoHTML;
 
 
 console.log('prueba')
