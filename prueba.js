37ea9d005e6e335f529e9040f32e253f40413204
dev12Tecclas <129604075+dev12Tecclas@users.noreply.github.com>
2023-05-25 11:07:39 -0500

    asd

--- prueba.js
+++ prueba.js
@@ -1,4 +1,14 @@
 window.alert('prueba')
 
 
+document.addEventListener('DOMContentLoaded', function (){
+    let body = document.body
+    let newDiv = document.createElement('div')
+    newDiv.innerHTML = 'hola soy germna'
+
+    body.appendChild(newDiv)
+
+})
+
+
 console.log('prueba')
