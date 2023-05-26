cfc45ac4935ee8710c40f418e1d7f00f8ee81017
dev12Tecclas <129604075+dev12Tecclas@users.noreply.github.com>
2023-05-26 11:01:11 -0500

    asd

--- extension.php
+++ extension.php
@@ -1,4 +1,4 @@
-<?php 
+<!-- <?php 
 /**
  * Plugin Name: prueba
  * Description: Una descripción de tu extensión.
@@ -43,12 +43,32 @@
 // echo "Consultas: " . $queryString;
 
 // Enqueue el script de mi-plugin.js
-function mi_plugin_enqueue_scripts() {
-  // wp_enqueue_script('mi-plugin', plugins_url('prueba.js', __FILE__), array('backbone'), '1.0', true);
-  wp_enqueue_script('mi-plugin', plugins_url('./react-prueba/src/main.js', __FILE__), array(), '1.0', true);
+// function mi_plugin_enqueue_scripts() {
+//   // wp_enqueue_script('mi-plugin', plugins_url('prueba.js', __FILE__), array('backbone'), '1.0', true);
+//   wp_enqueue_script('mi-plugin', plugins_url('./react-prueba/src/main.js', __FILE__), array(), '1.0', true);
+// }
+// add_action('wp_enqueue_scripts', 'mi_plugin_enqueue_scripts');
+
+
+
+?> -->
+
+<?php
+/**
+ * Plugin Name: Mi Complemento de React
+ * Plugin URI: https://www.ejemplo.com/
+ * Description: Un complemento que renderiza una aplicación de React en WordPress.
+ * Version: 1.0.0
+ * Author: Tu Nombre
+ * Author URI: https://www.tu-sitio.com/
+ */
+
+// Registra los scripts y estilos necesarios para tu aplicación de React
+function mi_complemento_enqueue_scripts() {
+    // Registra el archivo JavaScript compilado de tu aplicación de React
+    wp_enqueue_script( 'mi-complemento-react', plugin_dir_url( __FILE__ ) . './react-prueba/dist/assets/index-640d5fac.js', array(), '1.0.0', true );
+
+    // Registra los estilos CSS de tu aplicación de React
+    wp_enqueue_style( 'mi-complemento-react', plugin_dir_url( __FILE__ ) . './react-prueba/dist/assets/index-d526a0c5.css', array(), '1.0.0' );
 }
-add_action('wp_enqueue_scripts', 'mi_plugin_enqueue_scripts');
-
-
-
-?>
+add_action( 'wp_enqueue_scripts', 'mi_complemento_enqueue_scripts' );
